7f1157ebf23e51a8063aef17c341f2804eb524d4
Volodia <volodymyr.m@eivolo.com>
2021-10-12 17:24:24 +0300

    implemented quering for TypeOrm

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
 import { AuthService } from '../auth/auth.service';
+import {
+  ACCESS_DENIED,
+  BAD_REQUEST,
+  INTERNAL_SERVER_ERROR,
+  NOT_FOUND_ERROR,
+  UNAUTHORIZED,
+} from '../infrastructure/app-constants';
+import { UserDto } from './dto/user.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Get()
-  getUsers() {
-    return this.authService.findAll();
+  async getUsers(): Promise<UserDto[]> {
+    try {
+      const users = await this.authService.findAll();
+      return users;
+    } catch {
+      throw new HttpException(
+        INTERNAL_SERVER_ERROR,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  @Get(':id')
+  getUserById() {
+    return 'UserDto by id';
   }
 }
